0a9b805e0d0a0c0d42cab9f5a14a063b036d5ad3
deeno bahri <deenobahri12@gmail.com>
2018-12-06 14:20:03 -0600

    commit

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -23,22 +23,17 @@ class About extends Component {
                     <hr style={{borderTop: '3px solid #833fb2'}} />
                     
                     <p style={{fontSize: '20px', fontFamily: 'Arial, Helvetica, sans-serif'}}>
-                        Hello, my name is Deeno Bahri. I am Minneapolis, Minnesota born and raised. I come from an intense and heavily disciplined fitness background. It all started
-                        when I got a job as a front desk staff member at Los Campeones Gym, located in south Minneapolis. Soon after working as a front desk staff member, I 
-                        gradually transitioned into a personal trainer/strength coach. I continue to train clients every week because it is another huge passion of mine.
+                    Since I was young, electronics and technology intrigued me. I was always curious of the process and the mechanics that went into creating my smart phone, or my Play Station, or the websites I would browse. Upon finishing high school, I wasn’t sure as to what my focus of study should be. 
+                    Initially, I thought my passion was personal training and fitness. At the age of 17, I successfully created Optimal Health & Performance which is my personal training and strength coaching business. While I come from a disciplined fitness background and enjoy personal training, it never provided me with the challenge or satisfaction I craved. 
+                    Although this was my first professional experience, I started to seek out other avenues to creatively express myself. 
                     </p>
 
                     <p style={{fontSize: '20px', fontFamily: 'Arial, Helvetica, sans-serif'}}>
-                    All while pursuing my passion as a personal trainer and strength coach, I finally made the biggest decision of my life thus far, that is to dive deep into programming and software development fundamentals in 2016.
-                    I always had a huge passion for technology and electronics. A close friend of mine introduced me to programming and I was hooked instantly. Once I was exposed to programming, I never considered making it an actual career for myself. 
-                    But after hearing about other successful developer stories, and through words of encouragement, I made the decision to pursue the field of software and become a software engineer.
-                    The first language I learned basic programming with was Python. I got comfortable with all the basic concepts in programming such as variables, while loops, for-loops,
-                    logical operators, etc.. all in my first year of serious studying.
+                        Still in production....
                     </p>
+
                     <p style={{fontSize: '20px', fontFamily: 'Arial, Helvetica, sans-serif'}}>
-                    After spending alot of time learning basic programming logic, I made the life changing decision to enroll into the full stack Coding Boot Camp at the University of Minnesota.
-                    There, everything came together for me and clicked. I learned all around software development, everything from the front end UI, backend functionality, databases, and serverside development.
-                    I love to learn and won't stop until the job is done. Programming will forever be a major part of my life and I wouldn't have it any other way! 
+                    
                     </p>
                     </Cell>
 
