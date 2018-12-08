fd1ce554997c58201546bede9f3c536d8d3b7fac
deeno bahri <deenobahri12@gmail.com>
2018-12-07 23:37:19 -0600

    Updates

--- precache-manifest.ee97bc2562bc1a6e42e694e836463138.js
+++ precache-manifest.ee97bc2562bc1a6e42e694e836463138.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/New-Resume/static/js/runtime~main.6cc8b17d.js"
   },
   {
-    "revision": "968d4badb358f27c8423",
-    "url": "/New-Resume/static/js/main.968d4bad.chunk.js"
+    "revision": "24c4ba03a51cd1edd409",
+    "url": "/New-Resume/static/js/main.24c4ba03.chunk.js"
   },
   {
     "revision": "5fbbe378d4d42767c3bc",
     "url": "/New-Resume/static/js/1.5fbbe378.chunk.js"
   },
   {
-    "revision": "968d4badb358f27c8423",
+    "revision": "24c4ba03a51cd1edd409",
     "url": "/New-Resume/static/css/main.8e059d3e.chunk.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/New-Resume/static/css/1.16ddc00b.chunk.css"
   },
   {
-    "revision": "f8b9852b4d283c301777f84bc02df239",
+    "revision": "d31884a9db07bf1df94c5e50b25ffab7",
     "url": "/New-Resume/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/New-Resume/precache-manifest.ebfcb35597095e7a4d906778d9e39182.js"
+  "/New-Resume/precache-manifest.ee97bc2562bc1a6e42e694e836463138.js"
 );
 
 workbox.clientsClaim();
